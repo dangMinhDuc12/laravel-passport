bd0beba5c717703044c684bd5964409c71fae844
Duc Dang <dangminhduca3@gmail.com>
2022-01-08 16:11:10 +0700

    done flow reset password api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Mail\ForgotPassword;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -14,6 +19,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         try {
+            //Check email vs password valid
             if(Auth::attempt($request->only('email', 'password'))) {
                 $user = Auth::user();
                 $token = $user->createToken('app')->accessToken;
@@ -41,6 +47,8 @@ class AuthController extends Controller
     {
         try {
             $validated = $request->validated();
+
+            //create user & token
             $user = User::create([
                'name' => $validated['name'],
                'email' => $validated['email'],
@@ -60,5 +68,89 @@ class AuthController extends Controller
                 'message' => $exception->getMessage()
             ], 500);
         }
+    }
+
+    public function forgotPassword(Request $request)
+    {
+        //validate
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email'
+        ]);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $validator->errors()
+            ], 400);
+        }
+        $email = $request->email;
+
+        //check email exist
+        if(User::where('email', $email)->doesntExist()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Invalid Email'
+            ], 401);
+        }
+
+        // Generate Random Token
+        $token = rand(10, 100000);
+        try {
+            //insert email & token to password_resets table
+            DB::table('password_resets')->insert([
+                'email' => $email,
+                'token' => $token,
+                'created_at' => Carbon::now()
+            ]);
+
+            //send mail to user
+            Mail::to($email)->send(new ForgotPassword($token));
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Request Password Sent To Your Email'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $exception->getMessage()
+            ], 500);
+        }
+    }
+
+    public function resetPassword(Request $request)
+    {
+        //validate
+        $validator = Validator::make($request->all(), [
+            'token' => 'required',
+            'password' => 'required|min:6|confirmed'
+        ]);
+        if($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $validator->errors()
+            ], 400);
+        }
+        $validated = $validator->validated();
+
+        //check token valid
+        if(DB::table('password_resets')->where('token', $validated['token'])->doesntExist()) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Token Invalid'
+            ], 400);
+        }
+
+        //change password & delete token record
+        $email = DB::table('password_resets')->where('token', $validated['token'])->select('email')->first()->email;
+        User::where('email', $email)->update([
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        DB::table('password_resets')->where('token', $validated['token'])->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Password Changed Successfully'
+        ]);
     }
 }
